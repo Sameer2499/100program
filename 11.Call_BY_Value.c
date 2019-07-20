93caec4b93937291aa3c8409f7de09610826cc6d
Sameer Panat <sameerpanat99@gmail.com>
2019-07-20 21:36:33 +0530

    Create 11.Call_BY_Value.c

---
+++ 11.Call_BY_Value.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void swap(int &a,int &b)
+{
+	int tmp=a;
+	a=b;
+	b=tmp;
+	//printf("\nAfter swaping : %d %d",a,b);
+}
+int main()
+{  int a,b;
+   printf("Enter Numbers:");
+   scanf("%d%d",&a,&b);
+   printf("Before Swapping:%d %d",a,b);
+   swap(a,b);
+   printf("\nAfter Swapping:%d %d",a,b);
+   return 0;	
+}
